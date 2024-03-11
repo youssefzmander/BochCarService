b9fd5c7cdaf377162a7e9c3b6c3fb92323643e22
youssefzmander <youssef.zmander@gmail.com>
2024-03-11 23:05:07 +0100

    lookUp

---
+++ lib/services/lookUp.dart
@@ -0,0 +1,23 @@
+
+class LookUp{
+
+String IconeService(String inputString) {
+  List<String> FindList = ['BOUGIE' , 'AMORTISSEUR' , 'EMBRAYAGE'   , 'POMPE', 'SOUPAPE'  , 'BOBINE'  , 'COMPRESSEUR' , 'LAMPE'  , 'FREIN'  , 'CHAINE'  , 'HUILE'  , 'TRIANGLES'  , 'TURBO'  , 'DIAGNOSTIC'  , 'BATTERIE'  , 'FILTRE'  , 'ROTULE' ];
+  
+
+  String containedWord = findContainedWord(FindList, inputString);
+  return containedWord;
+}
+
+
+String findContainedWord(List<String> list, String inputString) {
+  List<String> words = inputString.split(' ');
+  for (String word in words) {
+    if (list.contains(word)) {
+      var _world=word+".png";
+          return word;
+    }
+  }
+  return "service.png";
+}
+}
